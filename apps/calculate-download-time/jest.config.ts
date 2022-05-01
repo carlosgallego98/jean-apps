5cbc436028c90bbe80b8dcb44d956613f8d347d9
Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2022-05-01 11:22:07 -0500

    initial commit

---
+++ apps/calculate-download-time/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'calculate-download-time',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/calculate-download-time',
+};
